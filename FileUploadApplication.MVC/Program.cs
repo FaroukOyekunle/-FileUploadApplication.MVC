98294806313b5332072190f13e94fc332dc3726b
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-12-07 13:08:08 +0100

    Add Project files

--- FileUploadApplication.MVC/Program.cs
+++ FileUploadApplication.MVC/Program.cs
@@ -1,3 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileUploadApplication.MVC.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
